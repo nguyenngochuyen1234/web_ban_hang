4898735a610338559c510a387e5d8f05755e3777
PKshift05 <quang05012003@gmail.com>
2023-06-13 14:32:00 +0700

    them login, register

--- src/page/Register.js
+++ src/page/Register.js
@@ -1,14 +1,234 @@
 import React from 'react'
+import {AutoComplete,Button,Cascader,Checkbox,Col,Form,Input,InputNumber,Row,Select,} from 'antd';
+import { useState } from 'react';
 import { Outlet, Link } from "react-router-dom";
+const { Option } = Select;
+const residences = [
+  {
+    value: 'zhejiang',
+    label: 'Zhejiang',
+    children: [
+      {
+        value: 'hangzhou',
+        label: 'Hangzhou',
+        children: [
+          {
+            value: 'xihu',
+            label: 'West Lake',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    value: 'jiangsu',
+    label: 'Jiangsu',
+    children: [
+      {
+        value: 'nanjing',
+        label: 'Nanjing',
+        children: [
+          {
+            value: 'zhonghuamen',
+            label: 'Zhong Hua Men',
+          },
+        ],
+      },
+    ],
+  },
+];
+const formItemLayout = {
+  labelCol: {
+    xs: {
+      span: 24,
+    },
+    sm: {
+      span: 8,
+    },
+  },
+  wrapperCol: {
+    xs: {
+      span: 24,
+    },
+    sm: {
+      span: 24,
+    },
+  },
+};
+const tailFormItemLayout = {
+  wrapperCol: {
+    xs: {
+      span: 24,
+      offset: 0,
+    },
+    sm: {
+      span: 16,
+      offset: 8,
+    },
+  },
+};
 const Register = () => {
-
-  
-
+  const [form] = Form.useForm();
+  const onFinish = (values) => {
+    console.log('Received values of form: ', values);
+  };
+  const prefixSelector = (
+    <Form.Item name="prefix" noStyle>
+      <Select
+        style={{
+          width: 70,
+        }}
+      >
+        <Option value="86">+86</Option>
+        <Option value="87">+87</Option>
+      </Select>
+    </Form.Item>
+  );
+  const suffixSelector = (
+    <Form.Item name="suffix" noStyle>
+      <Select
+        style={{
+          width: 70,
+        }}
+      >
+        <Option value="USD">$</Option>
+        <Option value="CNY">¥</Option>
+      </Select>
+    </Form.Item>
+  );
+  const [autoCompleteResult, setAutoCompleteResult] = useState([]);
+  const onWebsiteChange = (value) => {
+    if (!value) {
+      setAutoCompleteResult([]);
+    } else {
+      setAutoCompleteResult(['.com', '.org', '.net'].map((domain) => `${value}${domain}`));
+    }
+  };
+  const websiteOptions = autoCompleteResult.map((website) => ({
+    label: website,
+    value: website,
+  }));
   return (
     <div className='flex flex-col justify-center items-center h-[100%]'>
-      <h1 className='text-[50px]'>login</h1>
-      <h1>Nếu đã có tài khoản</h1>
-        <Link to="/login" className='text-[#074ac7] text-[30px]'>Đăng nhập</Link>
+      <h1 className='text-[50px]'>SIGN IN</h1>
+      <Form
+      {...formItemLayout}
+      form={form}
+      name="register"
+      onFinish={onFinish}
+      initialValues={{
+        residence: ['zhejiang', 'hangzhou', 'xihu'],
+        prefix: '86',
+      }}
+      style={{
+        maxWidth: 1000,
+      }}
+      scrollToFirstError
+    >
+      <Form.Item
+        rules={[
+          {
+            type: 'First name',
+            message: 'The input is not valid first name!',
+          },
+          {
+            required: true,
+            message: 'Please input your first name!',
+          },
+        ]}
+      >
+        <Input placeholder='First name' />
+      </Form.Item>
+      <Form.Item
+        rules={[
+          {
+            type: 'Last name',
+            message: 'The input is not valid Last name!',
+          },
+          {
+            required: true,
+            message: 'Please input your Last name!',
+          },
+        ]}
+      >
+        <Input placeholder='Last name' />
+      </Form.Item>
+
+      <Form.Item
+        rules={[
+          {
+            type: 'email',
+            message: 'The input is not valid E-mail!',
+          },
+          {
+            required: true,
+            message: 'Please input your E-mail!',
+          },
+        ]}
+      >
+        <Input placeholder='email' />
+      </Form.Item>
+
+      <Form.Item
+        rules={[
+          {
+            required: true,
+            message: 'Please input your password!',
+          },
+        ]}
+        hasFeedback
+      >
+        <Input.Password  placeholder='Password' />
+      </Form.Item>
+
+      <Form.Item
+        name="confirm"
+        dependencies={['password']}
+        hasFeedback
+        rules={[
+          {
+            required: true,
+            message: 'Please confirm your password!',
+          },
+          ({ getFieldValue }) => ({
+            validator(_, value) {
+              if (!value || getFieldValue('password') === value) {
+                return Promise.resolve();
+              }
+              return Promise.reject(new Error('The new password that you entered do not match!'));
+            },
+          }),
+        ]}
+      >
+        <Input.Password placeholder='Confirm password' />
+      </Form.Item>
+
+      <Form.Item
+        name="phone"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your phone number!',
+          },
+        ]}
+      >
+        <Input
+          addonBefore={prefixSelector}
+          style={{
+            width: '100%',
+          }}
+          placeholder='0123456789'
+        />
+      </Form.Item>
+
+      <Button className='bg-[#010101] text-[#fff] w-[100%]'>SIGN UP</Button> 
+    
+      <div className='flex justify-between'>
+        
+        <h1>Nếu đã có tài khoản?</h1>
+        <Link to="/login" className='text-[#074ac7] text-blue-400 hover:border-b border-blue-400'>SIGN IN</Link>
+      </div>
+    </Form>
     </div>
   )
 }
